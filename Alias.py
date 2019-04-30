cb7b48e9a0e211bf395a2aa4fad0963823aaef92
m0rv4i <email>
2019-04-30 15:19:25 +0100

    Add aliases for common sharp commands

--- Alias.py
+++ Alias.py
@@ -14,6 +14,21 @@ py_alias = [
 # C# Implant
 cs_alias = [
     ["s","get-screenshot"],
+]
+
+# Parts of commands to replace if command starts with the key
+cs_replace = [
     ["safetydump", "run-exe SafetyDump.Program SafetyDump"],
-    ["seatbelt", "run-exe Seatbelt.Program Seatbelt all"]
+<<<<<<< Updated upstream
+    ["seatbelt", "run-exe Seatbelt.Program Seatbelt all"],
+    ["sharpup", "run-exe SharpUp.Program SharpUp"],
+    ["rubeus", "run-exe Rubeus.Program Rubeus kerberoast"],
+=======
+    ["seatbelt", "run-exe Seatbelt.Program Seatbelt"],
+    ["sharpup", "run-exe SharpUp.Program SharpUp"],
+    ["rubeus", "run-exe Rubeus.Program Rubeus"],
+>>>>>>> Stashed changes
+    ["sharpview", "run-exe SharpView.Program SharpView"],
+    ["sharphound", "run-exe Sharphound2.Sharphound Sharphound"],
+    ["watson", "run-exe Watson.Program Watson"]
 ]
