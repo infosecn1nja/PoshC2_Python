6fc1980a2d3636ce1d1b26a04fed6097f8a37eaa
m0rv4i <email>
2019-04-30 15:19:25 +0100

    Add aliases for common sharp commands

--- Config.py
+++ Config.py
@@ -7,7 +7,7 @@ PORT_NUMBER = 443 # This is the bind port
 
 POSHDIR = "/opt/PoshC2_Python/"
 ROOTDIR = "/opt/PoshC2_Project/"
-HostnameIP = "https://192.168.233.1"
+HostnameIP = "https://193.36.15.234"
 DomainFrontHeader = "" # example df.azureedge.net
 DefaultSleep = "5s"
 Jitter = 0.20
